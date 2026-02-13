3d1c1d4b46cb0fa804437fb79a1945fad905e16a
Kun Qin <kuqin@microsoft.com>
2026-02-12 22:43:45 -0800

    downgrade logging level

--- patina_mm_supervisor_core/src/privilege_mgmt/syscall_dispatcher.rs
+++ patina_mm_supervisor_core/src/privilege_mgmt/syscall_dispatcher.rs
@@ -707,7 +707,7 @@ impl SyscallDispatcher {
         let alloc_type = ctx.arg1 as AllocateType;
         let mem_type = ctx.arg2 as MemoryType;
         let page_count = ctx.arg3;
-        log::info!("ALLOC_PAGE: alloc_type={}, mem_type={}, count={}", alloc_type, mem_type, page_count);
+        log::trace!("ALLOC_PAGE: alloc_type={}, mem_type={}, count={}", alloc_type, mem_type, page_count);
 
         // Only BSP can allocate pages (AP allocating involves page table updates)
         if !crate::is_bsp() {
